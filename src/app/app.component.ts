13853c407ec84fb2e765059ef4f9788ee4fcb4cd
Dima_Vagner <myevropa1@gmail.com>
2024-01-21 14:52:05 +0200

    add _handleError

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FetchService } from './services/fetch.service';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'vega-client';
+  public response: any = {
+    data: 'Data',
+    errorMessage: 'errorMessage',
+  };
+  constructor(private _fetchService: FetchService) {}
+
+  public ngOnInit(): void {
+    this._fetchService.get('https://localhost:7001/api/Users').subscribe(console.log)
+  }
+
+  // onClick() {
+  //   console.log('onClick')
+  // }
 }
---
+++ src/app/services/fetch.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FetchService {
+  constructor(private _httpClient: HttpClient) {}
+
+  public get(url: string) {
+    return this._httpClient.get(url).pipe(catchError(this._handleError));
+  }
+
+  private _handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('An error occurred:', error.error);
+    } else {
+      console.error(`Backend returned code ${error.status}, body was: `, error.error);
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+}
